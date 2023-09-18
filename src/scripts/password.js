508990b926d7743503f4df746649fce922e58dcb
Wishrito <110767037+Wishrito@users.noreply.github.com>
2023-09-18 16:32:23 +0200

    test

---
+++ src/scripts/password.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    let text;
+    let person = prompt("Please enter your name:", "your name");
+    let password = prompt("Please enter your password:", "your password");
+    if (person == null || person == "" ) {
+        if (password == null || password == "") {
+            text = "invalid credentials.";
+        }
+    if (password == null || password == "") {
+        text = "please provide a password.";
+    } else {
+      text = "logged in successfully.";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
+}
